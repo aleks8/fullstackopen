11ba1f8f8262da17877dde950c3ef42870e11921
aleks8 <aleks.lata@gmail.com>
2025-02-26 23:35:46 -0500

    Update app.js

--- part5/blog-list-end-to-end-testing-s2 [5.18]/blog-list-backend/app.js
+++ part5/blog-list-end-to-end-testing-s2 [5.18]/blog-list-backend/app.js
@@ -42,6 +42,7 @@ app.use('/api/login', loginRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/blogs', blogsRouter)
 
+//part of 5.18 answer
 if (process.env.NODE_ENV === 'test') {  
   const testingRouter = require('./controllers/testing')  
   app.use('/api/testing', testingRouter)
