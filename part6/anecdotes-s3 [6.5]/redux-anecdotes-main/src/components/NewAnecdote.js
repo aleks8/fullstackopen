a57ce0c049f214a2773e929acb28b2ec09745d85
aleks8 <aleks.lata@gmail.com>
2025-03-08 21:27:50 -0500

    Add files via upload

---
+++ part6/anecdotes-s3 [6.5]/redux-anecdotes-main/src/components/NewAnecdote.js
@@ -0,0 +1,25 @@
+//don't use this code 
+/*import { useDispatch } from 'react-redux'
+import { createAnecdote } from '../reducers/anecdoteReducer'
+
+const NewAnecdote = () => {
+  const dispatch = useDispatch()
+
+  const addAnecdote = (event) => {
+    event.preventDefault()
+    console.log('event', event)
+    //const content = event.target.anecdote.value
+    //event.target.anecdote.value = ''
+    const content = 'test'
+    dispatch(createAnecdote(content))
+  }
+
+  return (
+    <form onSubmit={addAnecdote}>
+      <input name="anecdote" />
+      <button type="submit">create</button>
+    </form>
+  )
+}
+
+export default NewAnecdote*/
