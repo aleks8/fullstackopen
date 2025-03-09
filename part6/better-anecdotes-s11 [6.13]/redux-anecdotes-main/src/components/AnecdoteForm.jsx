f726bd3f569c09676e5cfb86420fca743aa78baf
aleks8 <aleks.lata@gmail.com>
2025-03-09 12:01:39 -0400

    Add files via upload

---
+++ part6/better-anecdotes-s11 [6.13]/redux-anecdotes-main/src/components/AnecdoteForm.jsx
@@ -0,0 +1,39 @@
+//6.7 answer
+import { useDispatch } from 'react-redux'
+import { createAnecdote } from '../reducers/anecdoteReducer'
+import { setNotification, removeNotification } from '../reducers/notificationReducer'
+
+const AnecdoteForm = () => {
+  const dispatch = useDispatch()
+
+  const addAnecdote = (event) => {
+    event.preventDefault()
+    console.log('event', event)
+    const content = event.target.anecdote.value
+    event.target.anecdote.value = ''
+    //const content = 'test'
+    dispatch(createAnecdote(content))
+    dispatch(setNotification('you added '+content))
+    setTimeout(() => {        
+      dispatch(removeNotification('Add an anecdote or vote'))      
+    }, 5000)
+  }
+
+  return (
+    <div>
+    <h2>create new</h2>
+    <form onSubmit={addAnecdote}>
+      <input name="anecdote" />
+      <button type="submit">create</button>
+    </form>
+    </div>
+  )
+}
+
+export default AnecdoteForm
+
+/*
+<div>
+    <h2>create new</h2>
+</div>
+*/
