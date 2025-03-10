b8d0bc60719f73b09089d5af99995430193dc092
aleks8 <aleks.lata@gmail.com>
2025-03-09 23:03:02 -0400

    Add files via upload

---
+++ part6/anecdotes-and-the-backend-s3 [6.16]/redux-anecdotes-main/src/App.jsx
@@ -0,0 +1,33 @@
+import { useEffect } from 'react'
+import AnecdoteForm from './components/AnecdoteForm'
+import AnecdoteList from './components/AnecdoteList'
+import Filter from './components/Filter'
+import Notification from './components/Notification'
+//import anecdoteService from './services/anecdotes'
+//import { setAnecdotes } from './reducers/anecdoteReducer'
+import { useDispatch } from 'react-redux'
+import { initializeAnecdotes } from './reducers/anecdoteReducer'
+
+//6.14 and 6.15 submissions are identical because solved both 
+const App = () => {
+  const dispatch = useDispatch()  
+
+  useEffect(() => {    
+    //6.16 answer
+    dispatch(initializeAnecdotes()) 
+  }, [])
+
+
+  return (
+    <div>
+      <h2>Anecdotes</h2>
+      <Notification /><p></p>
+      <Filter />
+      <AnecdoteList />
+      <AnecdoteForm />
+    </div>
+  )
+}
+
+export default App
+
