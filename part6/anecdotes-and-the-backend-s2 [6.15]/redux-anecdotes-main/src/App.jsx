cae68ea07da0684052d9f878ec86cdc0aa7af5f7
aleks8 <aleks.lata@gmail.com>
2025-03-09 22:45:25 -0400

    Add files via upload

---
+++ part6/anecdotes-and-the-backend-s2 [6.15]/redux-anecdotes-main/src/App.jsx
@@ -0,0 +1,31 @@
+import { useEffect } from 'react'
+import AnecdoteForm from './components/AnecdoteForm'
+import AnecdoteList from './components/AnecdoteList'
+import Filter from './components/Filter'
+import Notification from './components/Notification'
+import anecdoteService from './services/anecdotes'
+import { setAnecdotes } from './reducers/anecdoteReducer'
+import { useDispatch } from 'react-redux'
+
+//6.14 and 6.15 submissions are identical because solved both 
+const App = () => {
+  const dispatch = useDispatch()  
+  useEffect(() => {    
+    anecdoteService    
+      .getAll().then(anecdotes => dispatch(setAnecdotes(anecdotes)))  
+  }, [])
+
+
+  return (
+    <div>
+      <h2>Anecdotes</h2>
+      <Notification /><p></p>
+      <Filter />
+      <AnecdoteList />
+      <AnecdoteForm />
+    </div>
+  )
+}
+
+export default App
+
---
+++ part6/anecdotes-and-the-backend-s2 [6.15]/redux-anecdotes-main/src/components/AnecdoteForm.jsx
@@ -0,0 +1,43 @@
+//6.7 answer
+import { useDispatch } from 'react-redux'
+import { createAnecdote } from '../reducers/anecdoteReducer'
+import { setNotification, removeNotification } from '../reducers/notificationReducer'
+import anecdoteService from '../services/anecdotes'
+
+const AnecdoteForm = () => {
+  const dispatch = useDispatch()
+
+  const addAnecdote = async (event) => {
+    event.preventDefault()
+    console.log('event', event)
+    const content = event.target.anecdote.value
+    event.target.anecdote.value = ''
+    //const content = 'test'
+    //part of 6.15 answer -- 2 lines below 
+    const newAnecdote = await anecdoteService.createNew(content)    
+    dispatch(createAnecdote(newAnecdote))
+    //dispatch(createAnecdote(content))
+    dispatch(setNotification('you added '+content))
+    setTimeout(() => {        
+      dispatch(removeNotification('Add an anecdote or vote'))      
+    }, 5000)
+  }
+
+  return (
+    <div>
+    <h2>create new</h2>
+    <form onSubmit={addAnecdote}>
+      <input name="anecdote" />
+      <button type="submit">create</button>
+    </form>
+    </div>
+  )
+}
+
+export default AnecdoteForm
+
+/*
+<div>
+    <h2>create new</h2>
+</div>
+*/
