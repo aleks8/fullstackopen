b3b87b22dc4e8ca80fa4a81d2725cf77a8b64dd6
aleks8 <aleks.lata@gmail.com>
2025-03-12 11:12:59 -0400

    Add files via upload

---
+++ part6/exercise-6.23 [6.23]/query-anecdotes-main/src/components/Notification.jsx
@@ -0,0 +1,34 @@
+import { useContext, useState } from "react"
+import {useNotificationValue} from '../NotificationContext'
+
+const Notification = () => {
+  
+  const style = {
+    border: 'solid',
+    padding: 10,
+    borderWidth: 1,
+    marginBottom: 5
+  }
+  const notificationValue = useNotificationValue()
+  console.log('NOTIFICATIONHERE', notificationValue)
+  /*const notificationValue = useNotificationValue()
+  console.log('notificationValue', notificationValue)
+  //if (true) return null
+  if (!notificationValue) {
+    return (
+      <div style={style}>
+       Add anecdote or vote 
+    </div>
+    )
+  }*/
+  
+//{notification}
+//{notificationDisplay}
+  return (
+    <div style={style}>
+      {notificationValue}
+    </div>
+  )
+}
+
+export default Notification
