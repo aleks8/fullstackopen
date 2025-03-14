67f719cadf95ca1a1dd3bf27d34779ea960810f4
aleks8 <aleks.lata@gmail.com>
2025-03-14 10:32:12 -0400

    Add files via upload

---
+++ part7/anecdotes-and-hooks-s1 [7.4]/routed-anecdotes-main/src/hooks/index.js
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+
+
+export const useField = (name) => {
+  const [value, setValue] = useState('')
+
+  const onChange = (event) => {
+    //console.log('value', value)
+    setValue(event.target.value)
+  }
+
+  return {
+    name,
+    value,
+    onChange
+  }
+}
+
+// modules can have several named exports
+
+export const useAnotherHook = () => {
+  // ...
+}
