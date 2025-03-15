3d4bfe232bcbb92237727cd8575bfea1b50a43ff
aleks8 <aleks.lata@gmail.com>
2025-03-15 11:47:52 -0400

    Add files via upload

---
+++ part7/country-hook [7.7]/country-hook-main/src/hooks/index.js
@@ -0,0 +1,17 @@
+//they already add useCountry
+/*import { useState } from 'react'
+
+export const useCountry = (name) => {
+  const [value, setValue] = useState('')
+
+  const onChange = (event) => {
+    //console.log('value', value)
+    setValue(event.target.value)
+  }
+
+  return {
+    name,
+    value,
+    onChange
+  }
+}*/
