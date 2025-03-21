16587810ae1ae288837c02f17233a1b961fe92bb
aleks8 <aleks.lata@gmail.com>
2025-03-21 13:48:32 -0400

    Add files via upload

---
+++ part7/react-query-and-context-s2 [7.11 - query]/blog-list-react-query-context/bloglist-frontend/src/main.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { NotificationContextProvider } from './notificationContext';
+import { UserContextProvider } from './UserContext';
+import App from './App';
+import './index.css';
+
+const queryClient = new QueryClient()
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+<QueryClientProvider client={queryClient}>
+    <NotificationContextProvider>
+        <UserContextProvider>
+        <App />
+        </UserContextProvider>
+    </NotificationContextProvider>
+</QueryClientProvider>
+);
+
