b62a673a9206702f21b6b84fb7f19b4a236802a4
aleks8 <aleks.lata@gmail.com>
2026-04-26 14:01:16 -0400

    Add files via upload

---
+++ part9/body-mass-index[9.1]/bmiCalculator.ts
@@ -0,0 +1,26 @@
+
+const calculateBmi = (a: number, b: number) : string => {
+  let bmi: number = (b / (a**2)) * 10000
+  if (bmi < 18.5 ) {
+    return 'Underweight'
+  }
+  else if (bmi < 25) {
+    return 'Normal Weight'
+  }
+  else if (bmi < 30) {
+    return 'Overweight'
+  }
+  else {
+    return 'Obese'
+  }
+}
+
+try {
+  console.log(calculateBmi(180, 74));
+} catch (error: unknown) {
+  let errorMessage = 'Something went wrong: '
+  if (error instanceof Error) {
+    errorMessage += error.message;
+  }
+  console.log(errorMessage);
+}
